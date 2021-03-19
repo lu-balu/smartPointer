3b35afa661c3935abe69f8325a1e6ecb89239695
Elizaveta <eliza_bazanova@mail.ru>
2021-03-19 22:51:05 +0300

    перегружен оператор *

--- smartPointer.h
+++ smartPointer.h
@@ -6,52 +6,60 @@
 template <typename T>
 class SmartPointer {
 public:
-    SmartPointer(T data) : p(new T) {
-        *p = data;
-        std::cout << "Выделена память, записан элемент " << data << std::endl;
-    };
-
-    ~SmartPointer(){
-        if(p != nullptr){
-            std::cout << "Освобождена память. Удаляется элемент " << *p << std::endl;
-            delete p;
-        }
-    }
-
+    
+    SmartPointer(const T& data);
     SmartPointer(const SmartPointer<T> &smartPointer) = delete;
+    SmartPointer(SmartPointer<T> && smartPointer);
 
-    SmartPointer(SmartPointer<T> && smartPointer): p(smartPointer.p) {
-        std::cout << "Выполняется перенос элемента " << *p << std::endl;
-        smartPointer.p = nullptr;
-    }
+    ~SmartPointer();
+
+    T* get() { return p; }
 
     SmartPointer<T>& operator =(const SmartPointer<T> &smartPointer) = delete;
-
-    SmartPointer<T>& operator =(SmartPointer<T> && smartPointer){
-        if(&smartPointer == this){
-            return *this;
-        }
-
-        delete p;
-
-        p = smartPointer.p;
-        smartPointer.p = nullptr;
-
-        return *this;
-    }
-
-
-    T* get() {
-        return p;
-    }
-
-    T operator* (){
-        return *p;
-    }
-
+    SmartPointer<T>& operator =(SmartPointer<T> && smartPointer);
+    T& operator*();
 
 private:
     T* p;
 };
+
+template <typename T>
+SmartPointer<T>::SmartPointer(const T& data) : p(new T) {
+    *p = data;
+    std::cout << "Выделена память, записан элемент " << data << std::endl;
+};
+
+template <typename T>
+SmartPointer<T>::~SmartPointer(){
+    if(p != nullptr){
+        std::cout << "Освобождена память. Удаляется элемент " << *p << std::endl;
+        delete p;
+    }
+}
+
+template <typename T>
+SmartPointer<T>::SmartPointer(SmartPointer<T> && smartPointer): p(smartPointer.p) {
+    std::cout << "Выполняется перенос элемента " << *p << std::endl;
+    smartPointer.p = nullptr;
+}
+
+template <typename T>
+SmartPointer<T>& SmartPointer<T>::operator =(SmartPointer<T> && smartPointer){
+    if(&smartPointer == this){
+        return *this;
+    }
+
+    delete p;
+
+    p = smartPointer.p;
+    smartPointer.p = nullptr;
+
+    return *this;
+}
+
+template <typename T>
+T& SmartPointer<T>::operator* (){
+    return *p;
+}
 
 #endif // SMARTPOINTER_H
